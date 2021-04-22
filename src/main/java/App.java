e19807a38d73788ec52d3ccb75e481858b69b141
SyrnoLinanalin <funworld994@gmail.com>
2021-04-22 21:15:23 +0300

    Bean create

---
+++ src/main/java/App.java
@@ -0,0 +1,25 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld beanagain =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanagain.getMessage());
+        Cat mycat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(mycat.getName());
+        Cat yourcat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(yourcat.getName());
+        System.out.println("Это ссылка на один бин? " + (bean==beanagain));
+        System.out.println("Это ссылка на один бин? " + (mycat==yourcat));
+
+
+    }
+}
